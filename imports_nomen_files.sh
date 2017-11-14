5e1db0302bca465d8381c8b81394a87abc7cc6cd
Mica7812 <mm.menard44@gmail.com>
2017-11-14 19:26:40 +0100

    Update imports_nomen_files.sh

--- imports_nomen_files.sh
+++ imports_nomen_files.sh
@@ -1,7 +1,7 @@
 
-mongoimport --type csv --collection "Nomen_age" --file Nomen_age.csv --headerline
-mongoimport --type csv --collection "Nomen_ech" --file Nomen_ech.csv --headerline
-mongoimport --type csv --collection "Nomen_fume" --file Nomen_fume.csv --headerline
-mongoimport --type csv --collection "Nomen_region" --file Nomen_region.csv --headerline
-mongoimport --type csv --collection "Nomen_revenu" --file Nomen_revenu.csv --headerline
-mongoimport --type csv --collection "Nomen_sexe" --file Nomen_sexe.csv --headerline
+mongoimport --type csv --db entredoproj --collection "Nomen_age" --file Nomen_age.csv --headerline
+mongoimport --type csv --db entredoproj --collection "Nomen_ech" --file Nomen_ech.csv --headerline
+mongoimport --type csv --db entredoproj --collection "Nomen_fume" --file Nomen_fume.csv --headerline
+mongoimport --type csv --db entredoproj --collection "Nomen_region" --file Nomen_region.csv --headerline
+mongoimport --type csv --db entredoproj --collection "Nomen_revenu" --file Nomen_revenu.csv --headerline
+mongoimport --type csv --db entredoproj --collection "Nomen_sexe" --file Nomen_sexe.csv --headerline
