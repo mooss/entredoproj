24a6eaede5e79e16be520ffe05d4b2c9043f3278
Mica7812 <mm.menard44@gmail.com>
2017-11-14 19:25:15 +0100

    Update imports_main_files.sh

--- imports_main_files.sh
+++ imports_main_files.sh
@@ -1,9 +1,8 @@
 
-mongoimport --type csv --collection "Indiv" --file Table_indiv.csv --headerline
-mongoimport --type csv --collection "Menage" --file Table_menage_1.csv --headerline
-mongoimport --type csv --collection "Nutrition" --file Table_indnut.csv --headerline
+mongoimport --type csv --db entredoproj --collection "Indiv" --file Table_indiv.csv --headerline
+mongoimport --type csv --db entredoproj --collection "Menage" --file Table_menage_1.csv --headerline
+mongoimport --type csv --db entredoproj --collection "Nutrition" --file Table_indnut.csv --headerline
 
-mongoimport --type csv --collection "Repas" --file repas_monocle.csv --headerline
-mongoimport --type csv --collection "Conso" --file conso_monocle.csv --headerline
-mongoimport --type csv --collection "Nomenclature" --file Nomenclature_3.csv --headerline
-
+mongoimport --type csv --db entredoproj --collection "Repas" --file repas_monocle.csv --headerline
+mongoimport --type csv --db entredoproj --collection "Conso" --file conso_monocle.csv --headerline
+mongoimport --type csv --db entredoproj --collection "Nomenclature" --file Nomenclature_3.csv --headerline
